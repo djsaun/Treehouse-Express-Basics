f8bc116d615d4ac83e6171837bff2a56e6eae184
David Saunders <david@monkee-boy.com>
2017-08-24 09:42:58 -0500

    Set up Express server and added homepage and subpage routes

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`<h1>This is the homepage</h1>`);
+});
+
+app.get('/hello', (req, res) => {
+  res.send(`<h1>Hello there</h1>`);
+});
+
+app.listen(3000, () => {
+  console.log('Restarting application');
+});

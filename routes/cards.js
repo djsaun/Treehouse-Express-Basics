397ce1d0a18c6a13566daac5a4b50e50099e1f48
David Saunders <david@monkee-boy.com>
2017-08-26 15:27:29 -0500

    Added question/answer toggle link to card template; Show random card when navigating to /card url

--- routes/cards.js
+++ routes/cards.js
@@ -3,19 +3,30 @@ const router = express.Router();
 
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
+const numCards = cards.length;
+console.log(numCards);
 
 router.get('/:id', (req, res) => {
-  res.render(`card`, {
-    prompt: cards[req.params.id].question,
-    hint: cards[req.params.id].hint
-  });
+  const { side } = req.query;
+  const { id } = req.params;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+  const templateData = { id, text };
+
+  if (side === 'question') {
+    templateData.hint = hint;
+    templateData.side = 'answer';
+    templateData.sideDisplay = 'Answer';
+  } else {
+    templateData.side = 'question';
+    templateData.sideDisplay = 'Question';
+  }
+  res.render(`card`, templateData);
 });
 
 router.get('/', (req, res) => {
-  res.render(`card`, {
-    prompt: cards[0].question,
-    hint: cards[0].hint
-  })
+  const randomCard = Math.floor(Math.random() * (numCards - 1 + 1)) + 1;
+  res.redirect(`/cards/${randomCard}?side=question`);
 })
 
 module.exports = router;
